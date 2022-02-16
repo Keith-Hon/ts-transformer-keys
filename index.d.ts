fa417b037b4ccf18d94e85cec306b7d8b84a6f3a
Keith Hon <@users.noreply.github.com>
2022-02-16 13:09:01 +0800

    added requiredKeys

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,7 @@
 export function keys<T extends object>(): Array<keyof T>;
+
+type RequiredOnly<T> = {
+  [K in keyof T as T[K] extends Required<T>[K] ? K : never]: T[K];
+};
+
+export function requiredKeys<T extends object>(): Array<keyof RequiredOnly<T>>;
