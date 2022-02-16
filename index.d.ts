a997247cce466e0cac5f02cec7ce280442d0e40f
Keith Hon <@users.noreply.github.com>
2022-02-16 13:11:11 +0800

    added optionalKeys

--- index.d.ts
+++ index.d.ts
@@ -4,4 +4,9 @@ type RequiredOnly<T> = {
   [K in keyof T as T[K] extends Required<T>[K] ? K : never]: T[K];
 };
 
+type OptionalOnly<T> = {
+    [K in keyof T as T[K] extends Required<T>[K] ? never : K]: T[K];
+  };
+
 export function requiredKeys<T extends object>(): Array<keyof RequiredOnly<T>>;
+export function optionalKeys<T extends object>(): Array<keyof RequiredOnly<T>>;
